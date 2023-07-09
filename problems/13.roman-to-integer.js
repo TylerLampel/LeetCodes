595b3b391bd2698587e75aac3466447cdb2734ef
Tyler <tyler.d.lampel@gmail.com>
2023-07-09 07:37:11 -0400

    Roman to Integer Javascript

---
+++ problems/13.roman-to-integer.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=13 lang=javascript
+ *
+ * [13] Roman to Integer
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+  const sym = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    const cur = sym[s[i]];
+    const next = sym[s[i + 1]];
+
+    if (cur < next) {
+      result += next - cur;
+      i++;
+    } else {
+      result += cur;
+    }
+  }
+  return result;
+};
+// @lc code=end
