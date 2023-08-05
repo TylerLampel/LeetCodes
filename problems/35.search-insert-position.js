44f3a24dca31ffbf64f58b3b2d071c58f3ec99e7
Tyler <tyler.d.lampel@gmail.com>
2023-08-05 10:31:26 -0400

    Search input position, JavaScript

---
+++ problems/35.search-insert-position.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=35 lang=javascript
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let lo = 0,
+    hi = nums.length; // we might need to inseart at the end
+  while (lo < hi) {
+    // breaks if lo == hi
+    let mid = lo + Math.floor((hi - lo) / 2); // always gives the lower mid
+    if (target > nums[mid]) {
+      lo = mid + 1; // no way mid is a valid option
+    } else {
+      hi = mid; // it might be possibe to insert @ mid
+    }
+  }
+  return lo;
+};
+// @lc code=end
