3ccccec193505da7b0a10d0e02f76a528fab49f3
Tyler <tyler.d.lampel@gmail.com>
2023-08-07 18:40:21 -0400

    Length of Last Word, JavaScript

---
+++ problems/58.length-of-last-word.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=58 lang=javascript
+ *
+ * [58] Length of Last Word
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  return s.trim().split(" ").at(-1).length;
+};
+// @lc code=end
