e9be1b9582dba5dd8ea78237aac8f8bb0ba16b5a
Tyler <lampel13@gmail.com>
2023-12-20 13:03:54 -0500

    Range Sum Query Immutable, JavaScript

---
+++ problems/303.range-sum-query-immutable.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=303 lang=javascript
+ *
+ * [303] Range Sum Query - Immutable
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    this.nums = nums;
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(left, right) {
+    let sum = 0;
+    for(let i = left; i <= right; i++){
+        sum += this.nums[i];
+    }
+    return sum;
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
+// @lc code=end
+
