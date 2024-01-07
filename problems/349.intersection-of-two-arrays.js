6fffed03717e6f277d4fa660dbb4d4aeddc946cf
Tyler <lampel13@gmail.com>
2024-01-07 10:15:35 -0500

    Intersection of Two Arrays, JavaScript

---
+++ problems/349.intersection-of-two-arrays.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=349 lang=javascript
+ *
+ * [349] Intersection of Two Arrays
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    let set = new Set(nums1)
+    let result = new Set()
+    for(let i=0; i<nums2.length; i++) {
+        if(set.has(nums2[i])) result.add(nums2[i])
+    }
+    return Array.from(result)
+};
+// @lc code=end
+
